aa8fb9f807be2624353a9f7789d307211b24da5a
Ermakov Egor <egor4yt@gmail.com>
2026-03-24 20:59:44 +0700

    Fix typo in `EmployerVerificationType` seed data migration

---
+++ src/Launchpad/Launchpad.Persistence/Migrations/20260324131659_FixedTypoInEmployerVerificationTypeSeed.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Launchpad.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixedTypoInEmployerVerificationTypeSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "EmployerVerificationTypes",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Title",
+                value: "Частный рекрутер");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "EmployerVerificationTypes",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Title",
+                value: "Кадровое агентство");
+        }
+    }
+}
